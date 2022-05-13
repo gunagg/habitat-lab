aaa3d6fb0ca1627e90ab9628510745dda6d2c90f
Gunjan Aggarwal <gaggarwal32@sonny.cc.gatech.edu>
2022-05-13 06:00:37 -0400

    add attribute nav changes

--- scripts/run_batch_objectnav_eval.sh
+++ scripts/run_batch_objectnav_eval.sh
@@ -20,6 +20,8 @@ export MAGNUM_LOG=quiet
 
 EVAL_CKPT_PATH_DIR="/srv/flash1/gaggarwal32/data/ovrl_habitat_on_web/ovrl_objectnav_rgb_fintuned_best_model.pth"
 DATA_PATH="data/datasets/objectnav/mp3d/v1/{split}/{split}.json.gz"
+ATTRIBUTE_NAV_COLOR_DATA_PATH="data/datasets/attributenav/hm3d/v1-color/{split}/{split}.json.gz"
+ATTRIBUTE_NAV_MATERIAL_DATA_PATH="data/datasets/attributenav/hm3d/v1-material/{split}/{split}.json.gz"
 
 MASTER_ADDR=$(srun --ntasks=1 hostname 2>&1 | tail -n1)
 export MASTER_ADDR
@@ -34,5 +36,7 @@ srun python -u -m habitat_baselines.run \
 EVAL.USE_CKPT_CONFIG False \
 EVAL_CKPT_PATH_DIR $EVAL_CKPT_PATH_DIR \
 EVAL.SPLIT "val" \
-TASK_CONFIG.DATASET.DATA_PATH $DATA_PATH \
+NUM_PROCESSES 20 \
+NUM_ENVS 20 \
+TASK_CONFIG.DATASET.DATA_PATH $ATTRIBUTE_NAV_COLOR_DATA_PATH \
 
